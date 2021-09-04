9d760b0149ca35407fc098e26dffa84615642f58
UnoSD <1582987+UnoSD@users.noreply.github.com>
2021-09-04 18:08:48 +0100

    Simplify calculate

--- main.fsx
+++ main.fsx
@@ -5,83 +5,29 @@ type Component = {
     SLA : decimal
 }
 and Dependency =
-    | Simple of Component // AND dependency
-    | Redundant of Component list // OR dependency
+    | Direct of Component
+    | Distributed of Component list
 
-// Let's pretend we have two storage accounts for redundancy and Azure Function has retry mechanism to try the primary and go to secondary on failure
-let blobStorage = {
-    SLA = 99.99m
-    Dependencies = []
-//    Redundancy = 2
-//    RTO = None
-//    RPO = None
-}
+let sqlDatabase = {
+        SLA = 99.95m
+        Dependencies = []
+    }
 
-let formRecognizer = {
-    SLA = 99.5m
-    Dependencies = []
-//    Redundancy = 1
-//    RTO = None
-//    RPO = None
-}
-
-let azureFunction = {
-    SLA = 99.5m
-    Dependencies = [ Simple blobStorage; Simple formRecognizer ]
-//    Redundancy = 1
-//    RTO = None
-//    RPO = None
-}
-
-let azureAd = {
-    SLA = 99.999m
-    Dependencies = []
-//    Redundancy = 1
-//    RTO = None
-//    RPO = None
-}
-
-// TODO: What happens when both Azure Function and APIM have a dependency on blob, make sure we do the math right
-let apiManagement = {
-    SLA = 99.9m
-    Dependencies = [ Simple azureFunction; Simple azureAd; (*blobStorage*) ]
-//    Redundancy = 1
-//    RTO = None
-//    RPO = None
-}
-
-// Should we add components that do not affect the availability? App Insight where the app does not fail if it cannot log?
-
-// What if the redundancy is not just a duplicate service? What if we write to a db, but if it's unavailable, we save data to a queue? queue and db will have different SLA...
-
-let appSvc = {
+let appService = {
     SLA = 99.95m
-    Dependencies = [
-        {
-            SLA = 99.95m
-            Dependencies = []
-//            Redundancy = 1
-//            RTO = None
-//            RPO = None
-        }
-    ]
-//    Redundancy = 1
-//    RTO = None
-//    RPO = None
+    Dependencies = [ Direct sqlDatabase ]
 }
-
-let downProbability' sla =
-    100.0m - sla
-
-let downProbability component' =
-    downProbability' component'.SLA
 
 let rec calculateCompountSla entryComponent =
+    let dependencyDownProbability =
+        function
+        | Direct c       -> 100m - calculateCompountSla c
+        | Distributed cs -> failwith "Not implemented"
+
     match entryComponent.Dependencies with
-    | []         -> entryComponent.SLA // Now we need to consider also Redundancy
-    | components -> 100.0m - List.sumBy (calculateCompountSla >> downProbability') ({ entryComponent with Dependencies = [] } :: components)
+    | []         -> entryComponent.SLA
+    | components -> 100m - (Direct { entryComponent with Dependencies = [] } :: components |> List.sumBy dependencyDownProbability)
     
-    //| components -> entryComponent.SLA :: (List.map (calculateCompountSla) components) |> List.fold ((*)) 1.0m
     
-calculateCompountSla appSvc
+calculateCompountSla appService
 |> printfn "%A"
