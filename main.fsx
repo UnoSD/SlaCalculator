f7dc32ce2781b47d9e1c3407c29607758acfd366
UnoSD <1582987+UnoSD@users.noreply.github.com>
2021-09-04 17:53:16 +0100

    Add heterogeneous dependencies

--- main.fsx
+++ main.fsx
@@ -3,56 +3,56 @@ open System
 // A component can be a Load Balancer
 // A component can be multiple A/A instances, in that case, redundancy is > 1
 type Component = {
-    Dependencies : Component list
+    Dependencies : Dependency list
     SLA : decimal // option if a component does not have an SLA, what do we do? that should mean that the whole solution does not have one, unless we can ignore the component, should we add a "canGoDown flag?"
-    Redundancy : int
-    RTO : TimeSpan option
-    RPO : TimeSpan option
+//    Redundancy : int
+//    RTO : TimeSpan option
+//    RPO : TimeSpan option
 }
-//and Dependency =
-//    | Simple of Component
-//    | Redundant of Component list
+and Dependency =
+    | Simple of Component // AND dependency
+    | Redundant of Component list // OR dependency
 
 // Let's pretend we have two storage accounts for redundancy and Azure Function has retry mechanism to try the primary and go to secondary on failure
 let blobStorage = {
     SLA = 99.99m
     Dependencies = []
-    Redundancy = 2
-    RTO = None
-    RPO = None
+//    Redundancy = 2
+//    RTO = None
+//    RPO = None
 }
 
 let formRecognizer = {
     SLA = 99.5m
     Dependencies = []
-    Redundancy = 1
-    RTO = None
-    RPO = None
+//    Redundancy = 1
+//    RTO = None
+//    RPO = None
 }
 
 let azureFunction = {
     SLA = 99.5m
     Dependencies = [ blobStorage; formRecognizer ]
-    Redundancy = 1
-    RTO = None
-    RPO = None
+//    Redundancy = 1
+//    RTO = None
+//    RPO = None
 }
 
 let azureAd = {
     SLA = 99.999m
     Dependencies = []
-    Redundancy = 1
-    RTO = None
-    RPO = None
+//    Redundancy = 1
+//    RTO = None
+//    RPO = None
 }
 
 // TODO: What happens when both Azure Function and APIM have a dependency on blob, make sure we do the math right
 let apiManagement = {
     SLA = 99.9m
     Dependencies = [ azureFunction; azureAd; (*blobStorage*) ]
-    Redundancy = 1
-    RTO = None
-    RPO = None
+//    Redundancy = 1
+//    RTO = None
+//    RPO = None
 }
 
 // Should we add components that do not affect the availability? App Insight where the app does not fail if it cannot log?
@@ -65,14 +65,14 @@ let appSvc = {
         {
             SLA = 99.95m
             Dependencies = []
-            Redundancy = 1
-            RTO = None
-            RPO = None
+//            Redundancy = 1
+//            RTO = None
+//            RPO = None
         }
     ]
-    Redundancy = 1
-    RTO = None
-    RPO = None
+//    Redundancy = 1
+//    RTO = None
+//    RPO = None
 }
 
 let downProbability' sla =
