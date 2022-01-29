94f32092f7c7b9b9d2e3aa0478a8920d12eb03b7
UnoSD <1582987+UnoSD@users.noreply.github.com>
2022-01-29 23:32:14 +0100

    Refactor calculator

--- Fable/Calculator.fs
+++ Fable/Calculator.fs
@@ -32,70 +32,107 @@ let private row (name : string) (sla : string) (dependsOn : string) (entrypoint 
 let private card content =
     card content Html.none
 
-let private th (text : string) =
+let private tableColumn (text : string) =
     Html.tableHeader [ Html.text text ]
 
 let private tableHeader (content : ReactElement list) =
     Html.thead [ Html.tableRow content ]
 
-let totals model =
-    Level.level [ ]
-                [ Level.item [ Level.Item.HasTextCentered ]
-                             [ div [ ]
-                                   [ Level.heading [ ]
-                                                   [ str "Composite SLA" ]
-                                     Level.title   [ ]
-                                                   [ str <| sprintf "%f%%" model.CompositeSLA ] ] ]
-                  Level.item [ Level.Item.HasTextCentered ]
-                             [ div [ ]
-                                   [ Level.heading [ ]
-                                                   [ str "Number of components" ]
-                                     Level.title   [ ]
-                                                   [ str <| sprintf "%i" (List.length model.Components) ] ] ]
-                  Level.item [ Level.Item.HasTextCentered ]
-                             [ div [ ]
-                                   [ Level.heading [ ]
-                                                   [ str "Downtime per year" ]
-                                     Level.title   [ ]
-                                                   [ str <| sprintf "%f hours" ((100m - model.CompositeSLA) * 365m * 24m / 100m) ] ] ] ]
+let private totals model =
+    let level =
+        Level.level [ ]
+        
+    let downtimeHoursPerYear =
+        (100m - model.CompositeSLA) * 365m * 24m / 100m
+    
+    let tile name value =
+        [ Html.div [ Level.heading [ ]
+                                   [ str name ]
+                     Level.title   [ ]
+                                   [ str value ] ] ]
+        |> Level.item [ Level.Item.HasTextCentered ]
+    
+    level [
+        tile "Composite SLA" (sprintf "%f%%" model.CompositeSLA)
+        
+        tile "Number of components" (sprintf "%i" (List.length model.Components))
+        
+        tile "Downtime per year" (sprintf "%f hours" downtimeHoursPerYear)
+    ]
+
+let private componentsTable model =
+    let componentRow (comp : Component) =
+        let sla =
+            comp.SLA.ToString()
+            
+        let dependsOn =
+            comp.Dependencies
+            |> List.map (function
+                         | Distributed _  -> "db"
+                         | Direct      lx -> lx.Name)
+            |> String.concat ", "
+        
+        let isEntryPoint =
+            comp = model.EntryPoint.Value
+        
+        row comp.Name sla dependsOn isEntryPoint
+    
+    Html.table [
+        tableHeader [
+            tableColumn "Name"
+            tableColumn "SLA"
+            tableColumn "Depends on"
+            tableColumn "Entry point"
+        ]
+        
+        Html.tableBody (
+            List.map componentRow model.Components
+        )
+    ]
+
+let private dependenciesBox model =
+    Select.select [
+        Select.IsMultiple
+    ] [ select [ Multiple true
+                 Size 4. ] [
+        yield! model.Components |> List.map (fun c -> option [ Value c.Name ] [ str c.Name ])
+    ] ]
+
+let private button dispatch name event =
+    Button.button [
+        Button.OnClick (fun _ -> dispatch event)
+    ] [ str name ]
+
+let private textField dispatch isValid label icon event =
+    Input.text [
+        if isValid then () else Input.Color Color.IsDanger
+        Input.OnChange (fun ev -> ev.Value |> event |> dispatch)
+    ]
+    |> iconField label icon
 
 let calculatorCard model dispatch =
-    let isSlaValid, value =
-        Decimal.TryParse model.SLA
+    let button = button dispatch
+    let componentsTable = componentsTable model
+    let dependenciesBox = dependenciesBox model
+    let textField = textField dispatch
+    let isSlaValid = (Decimal.TryParse model.SLA |> fst)
+    let totalsBox = Box.box' [] [ totals model ]
+    let container (content : seq<ReactElement>) = card [ Html.div content ]
     
-    card [
-        Html.div [
-            Box.box' [] [ totals model ]
-            
-            iconField "Name" Icon.Atom (Input.text [
-                Input.OnChange (fun ev -> ev.Value |> ChangeName |> dispatch)
-            ])
-            iconField "SLA" Icon.Ambulance <| Input.text [
-                if not isSlaValid then Input.Color Color.IsDanger else ()
-                Input.OnChange (fun ev -> ev.Value |> ChangeSLA |> dispatch)
-            ]
-            Select.select [
-                Select.IsMultiple
-            ] [ select [ Multiple true
-                         Size 4. ] [
-                yield! model.Components |> List.map (fun c -> option [ Value c.Name ] [ str c.Name ])
-            ] ]
-            Button.button [
-                Button.OnClick (fun _ -> dispatch ClickAdd)
-            ] [ str "Add" ]
-            Button.button [] [ str "Export" ]
-            Button.button [] [ str "Import" ]
-            
-            Html.table [
-                tableHeader [
-                    th "Name"
-                    th "SLA"
-                    th "Depends on"
-                    th "Entry point"
-                ]
-                Html.tableBody [
-                    yield! (model.Components |> List.map (fun x -> row x.Name (x.SLA.ToString()) (x.Dependencies |> List.map (function | Distributed _ -> "db" | Direct lx -> lx.Name) |> String.concat ", ") (x = model.EntryPoint.Value)))
-                ]
-            ]
-        ]
+    container [
+        totalsBox
+        
+        textField true "Name" Icon.Atom ChangeName
+        
+        textField isSlaValid "SLA" Icon.Ambulance ChangeSLA
+        
+        dependenciesBox
+        
+        button "Add" ClickAdd
+        
+        Button.button [] [ str "Export" ]
+        
+        Button.button [] [ str "Import" ]
+        
+        componentsTable
     ]
